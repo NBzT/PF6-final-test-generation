7c26809030ca1a996fbab58f128fd73d8bc4900c
NBzT <nixondgomez100@hotmail.com>
2026-03-02 13:24:03 -0500

    elcodigo

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-def main():
-  print("Hello learners!")
+import json
+import requests
 
-if __name__=="__main__":
-  main()
+
+def buscar_dish(numero):
+
+    response = requests.get("https://api-colombia.com/api/v1/TypicalDish")
+    dishes = json.loads(response.content)
+
+
+    for dish in dishes:
+        if dish.get('id') == numero:
+          return {
+        "id": dish.get('id'),
+        "name": dish.get('name'),
+        "description": dish.get('description'),
+            }
+    return
+
+##________________________________________________________##
+def main():
+    print("Hello learners!")
+    print("Escoja el plato típico de su gusto\n")
+
+    try:
+        numero = int(input("Escoja su plato favorito ingresando un número del 1 al 68"))
+        info = buscar_dish(numero)
+        if info:
+            print("\nInformación de su plato escogido:")
+            for key, value in info.items():
+                print(f"{key.capitalize()}: {value}")
+        else:
+            print("Ingrese un valor del 1 al 68")
+    except ValueError:
+        print("Por favor ingrese un número válido.")
+
+if __name__ == "__main__":
+    main()
